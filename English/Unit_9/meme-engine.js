34cb63e27849abfb9388ac4a9ab7be306c9ded20
Afrach <achraftahiri@proton.me>
2026-04-24 15:22:04 +0100

    change readme.md

--- English/Unit_9/meme-engine.js
+++ English/Unit_9/meme-engine.js
@@ -1,16 +1,55 @@
 const memes = document.querySelectorAll('.meme');
+const sounds = document.querySelectorAll('.sound');
+
+let meme = null;
 
 document.addEventListener('keydown', e => {
     if (e.key === '1') {
-        memes[0].classList.add('meme-on');
+        showMeme(memes[0], 'fb');
     }
     if (e.key === '2') {
-        memes[1].classList.add('meme-on');
+        showMeme(memes[1]);
     }
     if (e.key === '3') {
-        memes[2].classList.add('meme-on');
+        showMeme(memes[2], 'boom');
+    }
+    if (e.key === '4') {
+        showMeme(memes[3]);
+    }
+    if (e.key === '5') {
+        showMeme(memes[4]);
     }
     if (e.key === 'x') {
-        document.querySelectorAll('.meme.meme-on').forEach(m => m.classList.remove('meme-on'));
+        showMeme(null);
     }
 });
+
+function showMeme(nextMeme, id) {
+    if (meme == nextMeme) {
+        meme.classList.remove('meme-on');
+        meme = null;
+        return;
+    }
+    if (meme != null) {
+        meme.classList.remove('meme-on');
+    }
+    if (nextMeme == null) {
+        meme = null;
+        return;
+    }
+
+    playSound(id);
+
+    meme = nextMeme;
+    meme.classList.add('meme-on');
+}
+
+function playSound(id) {
+    if (id == null) {
+        return;
+    }
+
+    const sound = document.getElementById(id);
+    sound.currentTime = 0;
+    sound.play();
+}
